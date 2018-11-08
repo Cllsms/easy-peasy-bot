3655df3f51d90a2c85c31da03a199b322eaa9151
Collin Sims <collin.sims@wendys.com>
2018-11-08 13:38:43 -0500

    initial commit of shimon_bot code

--- index.js
+++ index.js
@@ -10,11 +10,11 @@
 
 function onInstallation(bot, installer) {
     if (installer) {
-        bot.startPrivateConversation({user: installer}, function (err, convo) {
+        bot.startPrivateConversation({ user: installer }, function(err, convo) {
             if (err) {
                 console.log(err);
             } else {
-                convo.say('I am a bot that has just joined your team');
+                convo.say('I am shimon_bot and I just joined your team');
                 convo.say('You must now /invite me to a channel so that I can be of use!');
             }
         });
@@ -30,11 +30,11 @@ var config = {};
 if (process.env.MONGOLAB_URI) {
     var BotkitStorage = require('botkit-storage-mongo');
     config = {
-        storage: BotkitStorage({mongoUri: process.env.MONGOLAB_URI}),
+        storage: BotkitStorage({ mongoUri: process.env.MONGOLAB_URI }),
     };
 } else {
     config = {
-        json_file_store: ((process.env.TOKEN)?'./db_slack_bot_ci/':'./db_slack_bot_a/'), //use a different name if an app or CI
+        json_file_store: ((process.env.TOKEN) ? './db_slack_bot_ci/' : './db_slack_bot_a/'), //use a different name if an app or CI
     };
 }
 
@@ -66,11 +66,11 @@ if (process.env.TOKEN || process.env.SLACK_TOKEN) {
  * TODO: fixed b0rked reconnect behavior
  */
 // Handle events related to the websocket connection to Slack
-controller.on('rtm_open', function (bot) {
+controller.on('rtm_open', function(bot) {
     console.log('** The RTM api just connected!');
 });
 
-controller.on('rtm_close', function (bot) {
+controller.on('rtm_close', function(bot) {
     console.log('** The RTM api just closed');
     // you may want to attempt to re-open
 });
@@ -81,28 +81,42 @@ controller.on('rtm_close', function (bot) {
  */
 // BEGIN EDITING HERE!
 
-controller.on('bot_channel_join', function (bot, message) {
+controller.on('bot_channel_join', function(bot, message) {
     bot.reply(message, "I'm here!")
 });
 
-controller.hears('hello', 'direct_message', function (bot, message) {
+controller.hears('hello', 'direct_message', function(bot, message) {
     bot.reply(message, 'Hello!');
 });
 
+controller.hears('Wendys Unified Android.*', ['ambient'], function(bot, message) {
+    console.log(message);
+    var buildOutcome = message.match[0]; //match[1] is the (.*) group. match[0] is the entire group (open the (.*) doors).
+    if (buildOutcome.indexOf('Success') !== -1) {
+        return bot.api.reactions.add({
+            timestamp: message.ts,
+            channel: message.channel,
+            name: 'shipitparrot',
+        }, bot.reply(message, 'Ship It!'));
+    } else if (buildOutcome.indexOf('Back to normal') !== -1) {
+    	return bot.api.reactions.add({
+            timestamp: message.ts,
+            channel: message.channel,
+            name: 'ultrafastparrot',
+        });
+    } else {
+        return bot.api.reactions.add({
+            timestamp: message.ts,
+            channel: message.channel,
+            name: 'sombrero',
+        }, bot.reply(message, "<!here> ^^^ :alert:"));
+    }
+});
 
-/**
- * AN example of what could be:
- * Any un-handled direct mention gets a reaction and a pat response!
- */
-//controller.on('direct_message,mention,direct_mention', function (bot, message) {
-//    bot.api.reactions.add({
-//        timestamp: message.ts,
-//        channel: message.channel,
-//        name: 'robot_face',
-//    }, function (err) {
-//        if (err) {
-//            console.log(err)
-//        }
-//        bot.reply(message, 'I heard you loud and clear boss.');
-//    });
-//});
+controller.on('direct_mention', function(bot, message) {
+	bot.reply(message, "Yes?")
+});
+
+controller.on('interactive_message_callback', function(bot, message) {
+	console.log(message);
+});
